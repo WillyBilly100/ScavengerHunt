7165941196d0520512bcfa82d9c8306b3a75d959
WillyBilly100 <154639890+WillyBilly100@users.noreply.github.com>
2024-09-13 18:08:52 -0700

    First commit

---
+++ Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public TMP_Text dialogText;
+    //public TMP_Text Test;
+    public Image seekImage;
+    public GameObject npc;
+    public GameObject randomSpawn;
+    //public TMP_Text dialogText;
+
+    /*public void SetDialogue(string text){
+        if(dialogueText != null){
+            dialogueText.text = text;
+        }
+    }*/
+
+    [SerializeField]
+    private Sprite[] collectibleSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CollectibleUpdate(int item)
+    {
+
+    }
+
+    public void ShowBox(string dialog, int item)
+    {
+        if (npc.GetComponent<DialogOpen>().begin)
+        {
+            scatterCoins();
+        }
+    }
+
+    public void scatterCoins()
+    {
+        randomSpawn.GetComponent<RandomSpawn>().DistributeCollectibles();
+        npc.GetComponent<DialogOpen>().coinsScattered();
+    }
+}
